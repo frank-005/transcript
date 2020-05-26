3b8ebd3650fe7ed2decae8fd86fb1e97f7c28846
Franklin Ombui <frankombui1@gmail.com>
2020-05-26 17:34:32 +0300

    better, bugs still present. maybe one

--- app.py
+++ app.py
@@ -1,45 +1,44 @@
 def app():
-    
+
 
     def inputJob():
         jobsDone = int(input("How many jobs have you done today? "))
+        jobsDoneId =  []
+        totalMoneyEarned = []
         for x in range(int(jobsDone)):
             jobId = input("Enter the Job Id: ")
+            jobsDoneId.append(jobId)
             moneyEarned = float(input("How much did you earn on this job? "))
-        return jobId, moneyEarned, jobsDone
-
-     
-
-    def money(moneyEarned):
-        totalMoneyEarned = []
-        totalMoneyEarned.append(moneyEarned)   
+            totalMoneyEarned.append(moneyEarned)
+        return jobId, jobsDone, jobsDoneId, totalMoneyEarned
+        
+    def money(totalMoneyEarned):
         sumMoney = 0
         for i in totalMoneyEarned:
             sumMoney = sumMoney + i
         return sumMoney
 
-    def inputRating():
-        currentJobid = input("Job Id for job whose rating you want to give: ")
-        ratingForJob = int(input("What is the rating? "))
-        return currentJobid, ratingForJob
-    
-    def rating(jobId, currentJobid, ratingForJob, jobsDone):
-        jobsDoneId = []
-        jobsDoneId.append(jobId)
+    def inputRating(jobsDone):
+        for x in range(int(jobsDone)):
+            currentJobid = input("Job Id for job whose rating you want to give: ")
+            ratingForJob = int(input("What is the rating? "))
+            return currentJobid, ratingForJob
+
+    def rating(jobId, currentJobid, ratingForJob, jobsDone, jobsDoneId):
         ratings = []
+
         sumRatings = 0
         avgRatings = 0
         for x in range(int(jobsDone)):
             if currentJobid in jobsDoneId:
                 ratings.append(ratingForJob)
+
                 for rating1 in ratings:
                     sumRatings = sumRatings + rating1
                     avgRatings = sumRatings / len(ratings)
-                    print(avgRatings)
-                
             else:
                 print("The job Id is not in our records")
-                inputRating()
+                inputRating(jobsDone)
         return jobsDoneId, avgRatings
 
     def output(jobsDone, sumMoney, avgRatings):
@@ -48,10 +47,10 @@ def app():
         print("Your average rating for this period is: " + str(avgRatings))
 
     def main():
-        jobId, moneyEarned, jobsDone = inputJob()
-        sumMoney = money(moneyEarned)
-        currentJobid, ratingForJob = inputRating()
-        jobsDoneId, avgRatings = rating(jobId, currentJobid, ratingForJob, jobsDone)
+        jobId, jobsDone, jobsDoneId, totalMoneyEarned = inputJob()
+        sumMoney = money(totalMoneyEarned)
+        currentJobid, ratingForJob = inputRating(jobsDone)
+        jobsDoneId, avgRatings = rating(jobId, currentJobid, ratingForJob, jobsDone, jobsDoneId)
         output(jobsDone, sumMoney, avgRatings)
 
 
