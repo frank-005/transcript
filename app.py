3de4502fdc2ac6fc2c2b23755d4fba157804d243
Franklin Ombui <frankombui1@gmail.com>
2020-05-26 13:43:43 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def main():
+    def inputJob():
+        jobId = input("Enter the Job Id: ")
+        moneyEarned = input("How much did you earn on this job? ")
+        return jobId, moneyEarned
+        
+    def money(moneyEarned):
+        totalMoneyEarned = []
+        moneyEarned.append(totalMoneyEarned)
+
+if __name__ == '__main__':
+    main()
