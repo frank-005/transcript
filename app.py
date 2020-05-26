5e47616b66c0d4e319af9212cf2804b378e256ca
Franklin Ombui <frankombui1@gmail.com>
2020-05-26 20:14:12 +0300

    Finally fixed bug

--- app.py
+++ app.py
@@ -11,31 +11,32 @@ def app():
             moneyEarned = float(input("How much did you earn on this job? "))
             totalMoneyEarned.append(moneyEarned)
         return jobId, jobsDone, jobsDoneId, totalMoneyEarned
-        
+
     def money(totalMoneyEarned):
         sumMoney = 0
+        print(totalMoneyEarned)
         for i in totalMoneyEarned:
             sumMoney = sumMoney + i
         return sumMoney
 
     def inputRating(jobsDone):
+        ratings = []
         for x in range(int(jobsDone)):
             currentJobid = input("Job Id for job whose rating you want to give: ")
             ratingForJob = int(input("What is the rating? "))
-            return currentJobid, ratingForJob
+            ratings.append(ratingForJob)
+            print(ratings)
+        return currentJobid, ratings
 
-    def rating(jobId, currentJobid, ratingForJob, jobsDone, jobsDoneId):
-        ratings = []
-
+    def rating(jobId, currentJobid, ratings, jobsDone, jobsDoneId):
         sumRatings = 0
         avgRatings = 0
-        for x in range(int(jobsDone)):
+        for rating in ratings:
             if currentJobid in jobsDoneId:
-                ratings.append(ratingForJob)
-
-                for rating1 in ratings:
-                    sumRatings = sumRatings + rating1
-                    avgRatings = sumRatings / len(ratings)
+                sumRatings = sumRatings + rating
+                print(sumRatings)
+                avgRatings = sumRatings / len(ratings)
+                print(avgRatings)
             else:
                 print("The job Id is not in our records")
                 inputRating(jobsDone)
@@ -49,12 +50,9 @@ def app():
     def main():
         jobId, jobsDone, jobsDoneId, totalMoneyEarned = inputJob()
         sumMoney = money(totalMoneyEarned)
-        currentJobid, ratingForJob = inputRating(jobsDone)
-        jobsDoneId, avgRatings = rating(jobId, currentJobid, ratingForJob, jobsDone, jobsDoneId)
+        currentJobid, ratings = inputRating(jobsDone)
+        jobsDoneId, avgRatings = rating(jobId, currentJobid, ratings, jobsDone, jobsDoneId)
         output(jobsDone, sumMoney, avgRatings)
-
-
-
 
     main()
 app()
