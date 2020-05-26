04ba3ae5bda6291536873b103e5ceb2f630cc1a0
Franklin Ombui <frankombui1@gmail.com>
2020-05-26 14:02:20 +0300

    Progress

--- app.py
+++ app.py
@@ -7,6 +7,30 @@ def main():
     def money(moneyEarned):
         totalMoneyEarned = []
         moneyEarned.append(totalMoneyEarned)
+        sumMoney = 0
+        for i in totalMoneyEarned:
+            sumMoney = sumMoney + i
+        return sumMoney
+
+    def inputRating():
+        currentJobid = input("Job Id for job whose rating you want to give: ")
+        ratingForJob = input("What is the rating? ")
+        return currentJobid, ratingForJob
+    
+    def rating(jobId, currentJobid, ratingForJob):
+        jobsDone = []
+        jobId.append(jobsDone)
+        if currentJobid in jobsDone:
+            ratings = []
+            sumRatings = 0
+            for x in ratings:
+                sumRatings = sumRatings + x
+            return sumRatings
+        else:
+            print("The job Id is not in our records")
+            inputRating()
+
+
 
 if __name__ == '__main__':
     main()
